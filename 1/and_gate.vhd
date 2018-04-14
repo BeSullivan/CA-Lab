6636277786a02fa45cf98c9dc83ad57a633c9317
SaeedReza AlipourFard <saeedreza.alipour@gmail.com>
2018-04-14 20:13:35 +0430

    Add files via upload

---
+++ 1/and_gate.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:06:39 02/18/2018 
+-- Design Name: 
+-- Module Name:    and_gate - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
+Entity and_gate is
+Port(
+A, B: in std_logic;
+C : out std_logic
+);
+End entity and_gate;
+Architecture gatelevel of and_gate is
+Begin
+C <= A and B;
+End gatelevel;
+
